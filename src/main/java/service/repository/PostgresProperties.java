f8ef247c460e7f6ca604a77f48776fe034be2931
yurezz <zapisator@logan-club.ru>
2021-05-29 22:33:35 +0300

    Мелкие улучшения.

--- src/main/java/service/repository/PostgresProperties.java
+++ src/main/java/service/repository/PostgresProperties.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "spring.postgres")
+@ConfigurationProperties(prefix = "postgres")
 @Data
 public class PostgresProperties {
     String host;
